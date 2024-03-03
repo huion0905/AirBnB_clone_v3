96762630726c0c5e2f69063f6266505f03669eca
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-02 21:48:44 -0500

    add variable 'cache_id'

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,7 +7,9 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
+cache_id = uuid.uuid4()
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -37,7 +39,8 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
