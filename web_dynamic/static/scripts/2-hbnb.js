31a76681307b9839004986174d7010d144c85454
iShezmu <arteaga25610@gmail.com>
2024-03-03 19:12:11 -0500

    new: update API entry point and command lines.

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    let amenitiesChecked = {};
+  
+    $('div.amenities input[type="checkbox"]').change(function() {
+      let amenityId = $(this).data('id');
+      let amenityName = $(this).data('name');
+  
+      if ($(this).is(':checked')) {
+        amenitiesChecked[amenityId] = amenityName;
+      } else {
+        delete amenitiesChecked[amenityId];
+      }
+  
+      let amenitiesList = Object.values(amenitiesChecked).join(', ');
+      $('.amenities h4').text(amenitiesList);
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data, textStatus) {
+      if (textStatus === 'success' && data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }).fail(function() {
+      $('#api_status').removeClass('available');
+    });
+  });
+  
