2f9022b59ab97ed5f96215119465daabb4d49091
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-09 19:47:12 -0500

    add uud4

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-    let amenitiesChecked = {};
-  
-    $('div.amenities input[type="checkbox"]').change(function() {
-      let amenityId = $(this).data('id');
-      let amenityName = $(this).data('name');
-  
-      if ($(this).is('checked')) {
-        amenitiesChecked[amenityId] = amenityName;
-      } else {
-        delete amenitiesChecked[amenityId];
-      }
-  
-      let amenitiesList = Object.values(amenitiesChecked).join(', ');
-      $('.amenities h4').text(amenitiesList);
-    });
+$(() => {
+  $('input').css({'margin-right': '10px'}); // Set margin for all inputs
+  let amenities = {}; // Store all checked amenities
 
-    $.get('http://0.0.0.0:5001/api/v1/status/', function(data, textStatus) {
-      if (textStatus === 'success' && data.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
-    }).fail(function() {
-      $('#api_status').removeClass('available');
-    });
+  /**
+   * Listen if an input is checked. If so, store it in amenities
+   * If the input is unchecked by the user, make a filter to delete it
+   */
+  $("input:checkbox").on('change', function () {
+    if ($( this ).prop('checked')){
+      amenities[$( this ).data('id')] = $( this ).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', ')); // Join all checked amenities
   });
   
+  $.ajax({
+    type: "GET",
+    url: "http://127.0.0.1:5001/api/v1/status/",
+    dataType: "json"
+  }).done((data) => {
+    if (data.status === 'OK')
+      $("div#api_status").addClass('available');
+    else
+      $("div#api_status").removeClass('available');
+  });
+});
